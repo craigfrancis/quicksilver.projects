3fc067c44ff34719cbcca51cfed44dc41c7ab4b5
Craig Francis <craig@craigfrancis.co.uk>
2012-09-05 10:28:16 +0100

    Enable the QSPresetAdditions by default (enabled catalog)

---
+++ build/Projects.build/Release/Projects.build/Objects-normal/i386/ProjectsAction.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/WebServer/Projects/quicksilver.projects/Projects/Projects/ProjectsAction.m \
+  /Volumes/WebServer/Projects/quicksilver.projects/Projects/Projects/ProjectsAction.h
